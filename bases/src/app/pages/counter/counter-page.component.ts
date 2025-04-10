bb36e72eaeec20551121654f08fadaec7b9306ce
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-10 18:35:59 -0400

    signals, estructura del nuevo angular

---
+++ bases/src/app/pages/counter/counter-page.component.ts
@@ -0,0 +1,34 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  templateUrl: './counter-page.component.html',
+  styles: `
+    button {
+      padding: 5px;
+      margin: 5px 10px;
+      width: 75px;
+    }
+  `,
+  // changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CounterPageComponent {
+  counter = 10;
+  counterSignal = signal(10);
+
+  constructor() {
+    // setInterval(() => {
+    //   this.counterSignal.update(v => v +1);
+    //   console.log("Tick")
+    // }, 2000);
+  }
+
+  increaseBy(value: number) {
+    this.counter += value;
+    this.counterSignal.update(current => current + value);
+  }
+
+  resetCounter() {
+    this.counter = 0;
+    this.counterSignal.set(0);
+  }
+}
