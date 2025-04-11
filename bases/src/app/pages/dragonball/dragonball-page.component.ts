a032b2da72ddb7d4a0528edc0511a6925d780722
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-11 10:39:16 -0400

    for e if

---
+++ bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,25 @@
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
+
+@Component({
+  templateUrl: './dragonball-page.component.html',
+})
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Piccolo', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
+  ]);
+
+  // powerClasses = computed(() => {
+  //   return {
+  //     'text-danger': true,
+  //   }
+  // })
+}
